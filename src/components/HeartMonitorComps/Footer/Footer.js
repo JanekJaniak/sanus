6e8cccafd0697b75c7e423e0f5c312e38b4ac196
Janek_Janiak <janek.janiak@gmail.com>
2021-04-06 20:15:04 +0200

    Add button render in footer

--- src/components/HeartMonitorComps/Footer/Footer.js
+++ src/components/HeartMonitorComps/Footer/Footer.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import React from 'react';
 
-import Button from '../Button/Button'
+import Button from '../Button/Button';
 
-import styles from './footerStyles.module.css'
+import styles from './footerStyles.module.css';
+
+const buttons = [
+  {name: 'Add', type: 'add'},
+  {name: 'Graph', type: 'graph'},
+  {name: 'Avg', type: 'avg'}
+];
 
 const Footer = () => {
   return(
     <div className={styles.footer}>
-      <Button name='+'/>
-      <Button name='Graph'/>
-      <Button name='Med'/>
+      {buttons.map(button => (
+        <Button 
+          name={button.name} 
+          type={button.type}
+          key={button.type}
+        />))}
     </div>
   )
-}
+};
 
 export default Footer
