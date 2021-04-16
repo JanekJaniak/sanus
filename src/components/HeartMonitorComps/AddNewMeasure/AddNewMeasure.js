76b50ee55767abf270c5bffe8eec93ea52d9eb94
Janek_Janiak <janek.janiak@gmail.com>
2021-04-16 10:45:40 +0200

    Feat: change addNewMeasure to class component

--- src/components/HeartMonitorComps/AddNewMeasure/AddNewMeasure.js
+++ src/components/HeartMonitorComps/AddNewMeasure/AddNewMeasure.js
@@ -1,29 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import styles from './addNMStyles.module.css'
 
 import Input from '../../UI/Input/Input';
 import Button from '../../HeartMonitorComps/Button/Button';
 
-const addNewMeasure = (props) => {
+ class AddNewMeasure extends Component  {
+  constructor() {
+    super()
 
-  return(
-    <div className={styles.container}>
-      <Input label='Date' inputtype='input' type ='date'/>  
-      <Input label='Time' inputtype='input' type ='time'/>
-      <Input label='SYS' inputtype='input' type ='number'/>
-      <Input label='DIA' inputtype='input' type ='number'/>
-      <Input label='HR' inputtype='input' type ='number'/>
-      <Input label='Notes' inputtype='textarea'/>
-      <div className={styles.buttonContainer}>
-        <Button
-          name='Submit'
-          action='submit'
-          handler={props.handler}
-        />
+    this.state = {
+      id: 1,
+      date: '01.04.21',
+      time: '7:00',
+      systolic: '130',
+      diastolic: '80',
+      heartRate: '70',
+    }
+  }
+
+  render() {
+    return(
+      <div className={styles.container}>
+        <Input label='Date' inputtype='input' type ='date'/>  
+        <Input label='Time' inputtype='input' type ='time'/>
+        <Input label='SYS' inputtype='input' type ='number'/>
+        <Input label='DIA' inputtype='input' type ='number'/>
+        <Input label='HR' inputtype='input' type ='number'/>
+        <Input label='Notes' inputtype='textarea'/>
+        <div className={styles.buttonContainer}>
+          <Button
+            name='Submit'
+            action='submit'
+            handler={this.props.handler}
+          />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 };
 
-export default addNewMeasure
+export default AddNewMeasure
