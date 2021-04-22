2a57b5a5485211cc5f3b1bbfbd545a219ab675de
Janek_Janiak <janek.janiak@gmail.com>
2021-04-22 21:17:07 +0200

    Render modal on prop WIP

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../../hoc/Aux'
 import Backdrop from '../Backdrop/Backdrop'
 
 import styles from './modalStyles.module.css';
 
-const modal = (props) => (
-  <Aux>
-    <div 
-      className={styles.modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)'
-      }}
-    >
-      {props.children}
-    </div>
-    <Backdrop show={props.show}/>
-  </Aux>
-);
+class Modal extends Component {
+  constructor() {
+    super()
+  }
 
-export default modal
+  render() {
+    return(
+      <Aux>
+        <div 
+          className={styles.modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)'
+          }}
+        >
+          {this.props.children}
+        </div>
+        <Backdrop show={this.props.show}/>
+      </Aux>
+    )
+  }
+};
+
+
+export default Modal
